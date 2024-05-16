1a6a562f0f8824a66612710a4de83a15885dd8ae
Ivo Mynttinen <ivomynttinen@gmail.com>
2024-05-16 18:11:27 +0200

    style: home

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Ivo Mynttinen';
-export const SITE_DESCRIPTION = 'Ivo Mynttinen is a software developer and designer.';
+export const SITE_DESCRIPTION = 'Design engineer with a passion for interior design, and homelabbing.';
