a235c2ed660938c9a7ecde1fcb164152ad26f212
Ivo Mynttinen <ivomynttinen@gmail.com>
2024-09-30 16:49:08 +0200

    fix: matomo tracking

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from 'astro/config';
-import matomo from 'astro-matomo';
 import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
@@ -7,18 +6,13 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ivomynttinen.com',
+  trailingSlash: 'ignore',
+  build: {
+    inlineStylesheets: 'always',
+  },
   integrations: [
     mdx(),
-    sitemap(),
-    matomo({
-      enabled: import.meta.env.PROD, // Only load in production: 
-      host: "https://matomo.hanglage-no8.de/",
-      setCookieDomain: "*.ivomynttinen.com",
-      siteId: 3,
-      heartBeatTimer: 5,
-      disableCookies: true,
-      debug: false,
-    }),
+    sitemap()
   ],
   devToolbar: {
     enabled: false
